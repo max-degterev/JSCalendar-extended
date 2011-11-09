9cde476c597fbcd9278524666cdc13edcaaedd73
Max Degterev <me@maxdegterev.name>
2011-11-10 00:36:07 +0400

    Small push

--- static/script.js
+++ static/script.js
@@ -15,9 +15,16 @@ $('.calendar').append(Calendar.generate({
 var O = {};
 
 O.calendarSlider = function() {
-    var cal = $('#calendar')
-        control = cal.find('.controls'),
+    var cont = $('#calendar'),
+        calendar = cont.find('.calendar'),
+        control = cont.find('.controls'),
         slider = control.find('.slider');
+        
+    var c_height = calendar.height(),
+        s_height = control.height(),
+        q = (s_height / c_height);
+        
+    console.log(s_height, c_height, (s_height / c_height));
         
     slider.draggable({
         addClasses: false,
