54c9c6ac6d8fd63edbc47d1b8fe339021b860dd0
Max Degterev <max@ostrovok.ru>
2011-11-10 20:09:55 +0400

    Calendar finished

--- static/script.js
+++ static/script.js
@@ -16,19 +16,29 @@ var O = {};
 
 O.calendarSlider = function() {
     var cont = $('#calendar'),
-        calendar = cont.find('.calendar'),
+        calendar = cont.find('.calendar-list'),
         control = cont.find('.controls'),
-        slider = control.find('.slider');
+        slider = control.find('.slider'),
+        labels = calendar.find('li > strong'),
+        l_height = labels.eq(0).height();
         
-    var c_height = calendar.height() + cont.height() + slider.height(),
-        s_height = control.height(),
-        max = s_height - slider.height(),
-        q = (c_height / s_height),
-        offset = control.offset().top;
-        
+    var s_height = slider.height(),
+        max = control.height() - s_height,
+        max_calend = calendar.height() - cont.height(),
+        offset = control.offset().top,
+        q = max_calend / max;
+
     var checkPosition = function(pos) {
-        return Math.min(Math.max(pos, 0), max)
+        return Math.min(Math.max(pos, 0), max);
     };
+    
+    var stickyLabels = function(top) {
+        $.each(labels, function(key, el) {
+            el.style.marginBottom = Math.min(el.parentNode.offsetTop + top - l_height, 0) + 'px';
+        });
+    };
+    
+    stickyLabels(0);
         
     slider.draggable({
         addClasses: false,
@@ -37,18 +47,26 @@ O.calendarSlider = function() {
     });
     
     slider.bind('drag', function (e, ui) {
-        calendar.css({top: -slider[0].offsetTop * q});
+        var top = -(slider[0].offsetTop * q);
+
+        calendar.css({top: top});
+        stickyLabels(top);
     });
     
     control.bind('click', function(e) {
-        var m = checkPosition(e.pageY - offset);
+        var m = checkPosition(e.pageY - offset - (s_height / 2));
+
         slider.animate({top: m}, {duration: 200, queue: false});
-        calendar.animate({top: -m * q}, {duration: 200, queue: false});
+        calendar.animate({top: -m * q}, {duration: 200, queue: false, step: function(top) {
+            stickyLabels(top);
+        }});
+        
     });
     
     cont.bind('mousewheel', function(e, d) {
         var m = checkPosition((d > 0) ? (slider[0].offsetTop - 5) : (slider[0].offsetTop + 5));
-        slider.css({top: m}).trigger('drag')
+
+        slider.css({top: m}).trigger('drag');
     });
 };
 
